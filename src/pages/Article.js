183241fc0217881a08f3a9df2b3f3f0e1f2ff9e9
dicolancy <heartevil@vip.qq.com>
2020-03-17 18:12:00 +0800

    暂存版本v0.1

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,60 @@
 import React from 'react'
+import { article, articlexlsx } from '../util/api'
+import WechatSta from '../components/WechatSta'
+
+const columns = [
+    {
+        title: '标题',
+        dataIndex: 'title',
+        key: 'title',
+        width:380
+    },
+    {
+        title: '发布日期',
+        dataIndex: 'ref_date',
+        key: 'ref_date',
+    },
+    {
+        title: '图文位置',
+        dataIndex: 'msgindex',
+        key: 'msgindex',
+    },
+    {
+        title: '阅读量',
+        dataIndex: 'int_page_read_count',
+        key: 'int_page_read_count',
+    },
+    {
+        title: '分享次数',
+        dataIndex: 'share_count',
+        key: 'share_count',
+    },
+    {
+        title: '留言数',
+        dataIndex: 'comment_count',
+        key: 'comment_count',
+    },
+    {
+        title: '好看数',
+        dataIndex: 'like_count',
+        key: 'like_count',
+    },
+    {
+        title: '收藏数',
+        dataIndex: 'add_to_fav_count',
+        key: 'add_to_fav_count',
+    },
+    {
+        title: '全文阅读完成率',
+        dataIndex: 'target_user',
+        key: 'target_user',
+    },
+]
 
 export default props => {
     return (
-        <div>Article</div>
+        <div style={{ padding: '0 30px' }}>
+            <WechatSta columns={columns} dataapi={article} xlsxapi={articlexlsx} />
+        </div>
     )
 }
--- src/pages/Fans.js
+++ src/pages/Fans.js
@@ -1,12 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { Table, Tabs, Button, DatePicker } from 'antd'
-import { get } from '../util/request'
-import { wechat as wechatApi, fans, fansxlsx } from '../util/api'
-import dayjs from 'dayjs'
-import filedownload from 'js-file-download'
-
-const { TabPane } = Tabs
-const { RangePicker } = DatePicker
+import React from 'react'
+import { fans, fansxlsx } from '../util/api'
+import WechatSta from '../components/WechatSta'
 
 const columns = [
     {
@@ -61,107 +55,10 @@ const columns = [
     },
 ]
 
-const FansTable = props => {
-    const { wechat, date } = props
-    const [dataList, setDataList] = useState([])
-    const [loading, setLoading] = useState(0)
-    const [pagination, setPagination] = useState({
-        hideOnSinglePage: true,
-        pageSize: 20,
-        current: 1
-    })
-
-    const syncFansList = () => {
-        setLoading(1)
-        get(`${fans}/${wechat.id}`, {
-            params: {
-                pageSize: pagination.pageSize,
-                pageIndex: pagination.current - 1,
-                ...date
-            }
-        }).then(data => {
-            setLoading(0)
-            if (data) {
-                const { rows, rowsCount } = data
-                setDataList(rows || [])
-                setPagination({ ...pagination, total: rowsCount })
-            }
-        })
-    }
-
-    useEffect(syncFansList, [wechat.id, pagination.pageSize, pagination.current, date])
-
-    const tableChangeHandler = (page) => {
-        setPagination({ ...pagination, current: page.current })
-    }
-
-    return (
-        <Table size="small"
-            rowKey="日期"
-            pagination={pagination}
-            columns={columns}
-            dataSource={dataList}
-            loading={loading}
-            onChange={tableChangeHandler}
-        />
-    )
-}
-
 export default props => {
-    const [wechatList, setWechatList] = useState([])
-    const [activeWechatId, setActiveWechatId] = useState(0)
-    const [date, setDate] = useState({
-        begin_date: dayjs().add(-7, 'd').format('YYYY-MM-DD'),
-        end_date: dayjs().add(-1, 'd').format('YYYY-MM-DD')
-    })
-
-    const syncWechatList = () => {
-        get(wechatApi).then(data => {
-            setWechatList(data || [])
-            setActiveWechatId(data && data[0] && data[0].id)
-        })
-    }
-
-    useEffect(syncWechatList, [])
-
-    const dateChangeHandler = (dates, dateStrings) => {
-        const [begin_date, end_date] = dateStrings
-        setDate({ begin_date, end_date })
-    }
-
-    const downloadHandler = () => {
-        get(`${fansxlsx}/${activeWechatId}`, { params: date, responseType: 'arraybuffer' }).then(res => {
-            if (res) {
-                let blob = new Blob([res.data], { type: res.headers['content-type'] })
-                filedownload(blob, `export_fans_${date.begin_date}_${date.end_date}.xlsx`)
-            }
-        })
-    }
-
     return (
         <div style={{ padding: '0 30px' }}>
-            <Tabs defaultActiveKey={`${activeWechatId}`}
-                size="small"
-                onChange={key => { setActiveWechatId(key) }}
-                tabBarExtraContent={<>
-                    <RangePicker bordered={false} onChange={dateChangeHandler}
-                        defaultValue={[dayjs().add(-7, 'd'), dayjs().add(-1, 'd')]}
-                        ranges={{
-                            Yesterday: [dayjs().add(-1, 'd'), dayjs().add(-1, 'd')],
-                            'Last Week': [dayjs().add(-1, 'w').startOf('week'), dayjs().add(-1, 'w').endOf('week')],
-                            'This Week': [dayjs().startOf('week'), dayjs().endOf('week')],
-                            'This Month': [dayjs().startOf('month'), dayjs().endOf('month')],
-                        }} />
-                    <Button type="link" onClick={downloadHandler}>Download xlsx</Button>
-                </>}>
-                {
-                    wechatList.map(c => (
-                        <TabPane tab={c.name} key={c.id}>
-                            <FansTable date={date} wechat={c} />
-                        </TabPane>
-                    ))
-                }
-            </Tabs>
+            <WechatSta columns={columns} dataapi={fans} xlsxapi={fansxlsx} />
         </div>
     )
 }
